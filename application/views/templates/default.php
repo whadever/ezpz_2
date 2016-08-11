fb45033fc2989ce2996a6e7029b022675ce45af7
whadever <irpanwinata@gmail.com>
2016-08-11 08:43:27 +0700

    fixed js

--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -16,13 +16,11 @@
 
 
     <!-- Begin Scripts -->
-    <script src="<?php echo base_url() ?>js/jquery-1.11.0.min.js"></script>
-    <script src="<?php echo base_url() ?>js/jquery-3.1.0.js"></script>
 
 
-    <script src="<?php echo base_url() ?>js/jquery.waypoints.min.js"></script>
+
+    
 	<script src="<?php echo base_url() ?>js/bootstrap.min.js"></script>
-    <script src="<?php echo base_url() ?>js/flat-ui.min.js"></script>
     <script src="<?php echo base_url() ?>js/bootstrap-typeahead.js"></script>
     <script src="<?php echo base_url() ?>js/jquery.multi-select.js"></script>
   
@@ -134,6 +132,7 @@
 
 	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <script src="<?php echo base_url() ?>js/jquery.waypoints.min.js"></script>
     <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="<?php echo base_url(); ?>js/bootstrap.min.js"></script>
 
