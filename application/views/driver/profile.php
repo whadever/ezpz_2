4b40264ee66ea4f22dfbdd53077ac1e8c5dada7e
setyawan9 <setyawansusanto99@outlook.com>
2016-08-16 10:45:58 +0700

    profile

--- application/views/driver/profile.php
+++ application/views/driver/profile.php
@@ -1,8 +1,38 @@
-<div class="container-fluid">
-	<div class="col-md-2"></div>
-	<div class="col-md-8">
-		<?php echo $user->username ?>
-		<div class="profile-picture" style="background-image : url(<?php echo base_url().$user->photo ?>); width: 500px; height:500px;"></div>
+<div class="container-fluid" style="padding-top:20px;">
+	<div class="row">
+		<div class="col-md-2"></div>
+		<div class="col-md-8 text-center">
+			<div class="row">
+				<div class="profile-picture" style="background-image : url(<?php echo base_url().$user->photo ?>); " id="edit-prof-pic">
+				</div>
+			</div>
+			<div class="row">
+				<p id="profile-name"><?php echo $user->username ?></p>
+			</div>
+		</div>
+		<div class="col-md-2"></div>
 	</div>
+</div>
+
 	<div class="col-md-2"></div>
 </div>
+
+<script>
+
+var test = [""];
+<?php $i = 0; ?>
+
+<?php foreach ($lists as $list): ?>
+	test[<?php echo $i ?>] = "<?php echo $list->name ?>";
+	<?php $i++; ?>
+<?php endforeach; ?>
+
+$("#restaurant-search").typeahead({
+
+                        minLength: 0,
+                        items: 4,
+                        source: test,   
+                    });
+
+</script>
+
