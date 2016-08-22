13d4c5171f9dbd76fc716912374dfe7bb899b049
whadever <irpanwinata@gmail.com>
2016-08-22 16:36:29 +0700

    yey

--- application/views/client/home.php
+++ application/views/client/home.php
@@ -144,7 +144,7 @@
 							    				<th>No.</th>
 							    				<th>Photo</th>
 							    				<th>Name</th>
-							    				<th>Description</th>
+							    				<th class="hidden-xs">Description</th>
 							    				<th>Price</th>
 							    				<th>Action</th>
 							    			</tr>
@@ -156,7 +156,7 @@
 							    				<td><?php echo $i ?></td>
 							    				<td><img src="<?php echo base_url($dish->photo) ?>" width="50" alt=""></td>
 							    				<td><?php echo $dish->name ?></td>
-							    				<td><?php echo $dish->description ?></td>
+							    				<td class="hidden-xs" style="max-width: 200px"><div style="word-wrap: break-word; width: 80%"><?php echo $dish->description ?></div></td>
 							    				<td><?php echo NZD($dish->price) ?></td>
 							    				<td><span class="glyphicon glyphicon-pencil" aria-hidden="true" href="" data-toggle="modal" data-target="#edit_menu" data-id="<?php echo $dish->id?>" data-name="<?php echo $dish->name?>" style="cursor:pointer;""></span>
 							    				<span class="glyphicon glyphicon-trash" aria-hidden="true"></span>
