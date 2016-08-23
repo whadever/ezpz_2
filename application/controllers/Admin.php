c3756490181d68804827de59c03530a80efa85ed
felitas <felita.31895@gmail.com>
2016-08-23 11:52:28 +0700

    edit cuisine, needs fix on photos

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -265,6 +265,61 @@ class Admin extends CI_Controller{
 			redirect('admin/cuisines');
 		}
 	}
+//still needs fix on photos
+	public function edit_cuisine(){
+		if($this->input->post('update')){
+			$config['allowed_types']        = 'jpg|png';
+            $config['max_size']             = 10000;	
+            // $config['max_width']            = 1000;
+            // $config['max_height']           = 768;
+
+            
+								
+			$config['upload_path']          = 'images/cuisines';
+			$config['overwrite']			= False;
+			$this->upload->initialize($config);
+
+			//Check if the folder for the upload existed
+			if(!file_exists($config['upload_path']))
+			{
+				//if not make the folder so the upload is possible
+				mkdir($config['upload_path'], 0777, true);
+			}
+
+            if ($this->upload->do_upload('photo'))
+            {	
+            	$image = $this->upload->data();
+                //Get the link for the database
+                $photo = $config ['upload_path'] . '/' . $image['file_name'];
+                			//image_moo
+				$this->image_moo
+					->load($config ['upload_path'] . '/' . $image['file_name'])
+					->resize_crop(350,250)
+					->save_pa('','_thumb');
+
+				$this->image_moo
+				->load($config ['upload_path'] . '/' . $image['file_name'])
+				->resize_crop(1900,700)
+				->save($config ['upload_path'] . '/' . $image['file_name'],TRUE);
+
+
+            }
+            else{
+            	$photo='';
+            }
+
+			$data = array(
+				'id'	=> $this->input->post('id'),
+				'name'	=> $this->input->post('name'),
+				'photo' => $photo,
+				'thumb' => $config ['upload_path'] . '/' . $image['raw_name'].'_thumb'.$image['file_ext']
+
+			);
+			$this->crud_model->update_data('cuisines',$data,array('id' => $data['id']));
+
+			redirect('admin/cuisines');
+		}	
+	}
 
 	public function delete_cuisine(){
 		if($this->input->post('delete')){
