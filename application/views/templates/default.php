4f6302748e4c6eb6c40726ea7bd37dddc46b8075
setyawan9 <setyawansusanto99@outlook.com>
2016-08-24 11:18:27 +0700

    footer

--- application/views/templates/default.php
+++ application/views/templates/default.php
@@ -112,8 +112,8 @@
 
 <footer>
   <div class="container-fluid">
-    <div class="row">
-      <h3 class="text-center">&copy; Hassee Developers</h3>
+    <div class="row text-center" style="padding: 10px;">
+      <img src="<?php echo base_url()?>images/logo.png" width="50px" height="50px">&copy; Hassee 2016. All Rights Reserved under LRM Corporation
     </div>
   </div>
 </footer>
