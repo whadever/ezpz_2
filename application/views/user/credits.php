85e98be5bed8966d9e78304561c60d38496c7da6
felitas <felita.31895@gmail.com>
2016-09-02 10:59:26 +0700

    ceciiilia

--- application/views/user/credits.php
+++ application/views/user/credits.php
@@ -1,37 +1,57 @@
-
+<style type="text/css">
+  .input-group-addon{
+    background-color: #34495e;
+    border-radius: 0px;
+    border:3px solid #34495e;
+    color: #fff;
+  }
+  .table>tbody>tr>td, .table>tbody>tr>th, .table>tfoot>tr>td, .table>tfoot>tr>th, .table>thead>tr>td, .table>thead>tr>th {
+        padding: 8px;
+        line-height: 1.42857143;
+        vertical-align: top;
+        
+        border-color: #34495e;
+    }
+  
+</style>
 <div id="main">
-<div class="container-fluid">
+<div class="container">
     <div class="row">
         <div class="col-lg-2"></div>
-        <div class="col-lg-8">
-            <div class="cards">
-                <h3 align="center">We Accept </h3>
-            </div>
-            <?php echo form_open('user/topup', array('class' => 'inline')) ?>
-                <table class="table table-bordered">
-                    <tr>
-                        <td>Amount</td>
-                        <td>
+        <div class="col-lg-8" style="padding:20px;">
+          <div class="row">
+                <h2 align="center" style="margin:30px;">Top Up Wallet</h2>
+          </div>
+          <div class="row">
+            <table class="table">
+            <tr>
+                <th class="payhead"><strong>Current Balance</strong></th>
+                <td><?php echo NZD($this->crud_model->get_by_condition('users', array('id' => $this->session->userdata('user_id')))->row('credits')) ?></td>
+            </tr>
+            <tr>
+                  <th class="payhead" style="vertical-align: middle;"><strong>Add Balance</strong></th>
+                  <td>
+                            <?php echo form_open('user/topup', array('class' => 'inline')) ?>            
                             <div class="input-group">
                               <div class="input-group-addon">$</div>
                               <input type="number" name="amount" id="amount" class="form-control" placeholder="amount" required>
                               <div class="input-group-addon">.00</div>
                             </div>
-                        </td>
-                    </tr>
-                    <tr>
-                        <td colspan="2">
-                           <script
-                              src="https://checkout.stripe.com/checkout.js" class="stripe-button pull-right"
-                              data-key="pk_test_WHJeocUki4v9PeEmVbhYedqV"
-                              data-email="<?php echo $user_email ?>"
-                              data-name="EZPZ Food Delivery"
-                              data-description="Wallet Top Up"
-                              data-locale="auto">
-                            </script>
-                        </td>
-                    </tr>
-                </table>
+                  </td>
+            </tr> 
+            </table>           
+          </div>
+          <div class="row text-center" style="padding:20px;padding-right:5px;">               
+                   <script
+                      src="https://checkout.stripe.com/checkout.js" class="stripe-button pull-right"
+                      data-key="pk_test_WHJeocUki4v9PeEmVbhYedqV"
+                      data-email="<?php echo $user_email ?>"
+                      data-name="EZPZ Food Delivery"
+                      data-description="Wallet Top Up"
+                      data-locale="auto">
+                    </script>
+            </div>        
+                
             <?php echo form_close() ?>
         </div>
         <div class="col-lg-2"></div>
