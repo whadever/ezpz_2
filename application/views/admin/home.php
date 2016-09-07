6e8e95a90e75cd9b74fcdeee3845712d0ab5dd81
setyawan9 <setyawansusanto99@outlook.com>
2016-09-07 12:08:41 +0700

    homr

--- application/views/admin/home.php
+++ application/views/admin/home.php
@@ -1,4 +1,5 @@
 <div class="container">
+
   <div class="row">
     <div class="col-xs-12 text-center">
       <h2>Welcome to Reyner's God Page</h2>
@@ -31,21 +32,97 @@
     </div>
     <div class="col-lg-2"></div>
   </div>
+
   <div class="row text-center" style="margin-top:5%;">
       <a href="<?php echo base_url('admin/edit_settings'); ?>" class="btn btn-primary">
       <span class="glyphicon glyphicon-edit"></span> Edit Profile</a>            
   </div>
 
-  <div class="row">
-    <div class="col-xs-4">
-      <a href="<?php echo base_url('admin/drivers/0') ?>">Unapproved Drivers <span class="badge"> <?php echo count($unapproved_drivers) ?></span></a>
+    
+  <!-- <div class="row">
+
+    <table class="table">
+      <thead>
+        tr
+      </thead>
+    </table>
+    
+    <table class="table">
+      <thead>
+      <tr>
+        <th>
+          Unapproved Drivers <span class="badge"> <?php echo count($unapproved_drivers) ?></span>
+        </th>
+        <th>
+          Unapproved Clients <span class="badge"> <?php echo count($unapproved_drivers) ?></span>
+        </th>
+      </tr>
+      </thead>
+      <tbody>
+      <?php foreach ($unapproved_drivers as $drivers) : ?>
+      <tr>
+        <td>
+          
+        </td>
+      </tr>
+      <?php endforeach; ?>
+      </tbody>
+    </table>
+
+  </div> -->
+  
+  <div class="row text-center">
+      
+    <div class="col-lg-4">
+      <div class="row">
+        <div class="col-xs-12">
+          <a href="<?php echo base_url('admin/drivers/0') ?>">Unapproved Drivers <span class="badge"> <?php echo count($unapproved_drivers) ?></span></a>
+        </div>
+      </div>
+
+      <?php foreach($unapproved_drivers as $drivers): ?>
+        <div class="row">
+          <div class="col-xs-8">
+            <?php echo $drivers->firstname.' '.$drivers->lastname ?>
+          </div>
+          <div class="col-xs-4">approve gak</div>
+        </div>
+      <?php endforeach; ?>
     </div>
-    <div class="col-xs-4">
-      <a href="<?php echo base_url('admin/clients/0') ?>">Unapproved Clients <span class="badge"> <?php echo count($unapproved_clients) ?></span></a>
+    <div class="col-lg-4">
+      <div class="row">
+        <div class="col-xs-12">
+          <a href="<?php echo base_url('admin/clients/0') ?>">Unapproved Clients <span class="badge"> <?php echo count($unapproved_clients) ?></span></a>
+        </div>
+      </div>
+
+      <?php foreach($unapproved_clients as $clients): ?>
+        <div class="row">
+          <div class="col-xs-8">
+            <?php echo $clients->name ?>
+          </div>
+          <div class="col-xs-4">approve gak</div>
+        </div>
+      <?php endforeach; ?>
+
     </div>
-    <div class="col-xs-4">
-      <a href="#">Unapproved Drivers <span class="badge"> <?php echo count($unapproved_drivers) ?></span></a>
+    <div class="col-lg-4">
+      <div class="row">
+        <div class="col-xs-12">
+          <a href="<?php echo base_url('admin/drivers/0') ?>">Unapproved Drivers <span class="badge"> <?php echo count($unapproved_drivers) ?></span></a>
+        </div>
+      </div>
+
+      <?php foreach($unapproved_drivers as $drivers): ?>
+        <div class="row">
+          <div class="col-xs-8">
+            <?php echo $drivers->firstname.' '.$drivers->lastname ?>
+          </div>
+          <div class="col-xs-4">approve gak</div>
+        </div>
+      <?php endforeach; ?>
     </div>
+
   </div>
 </div>
 
