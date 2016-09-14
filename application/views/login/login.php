c96c014f17e91344a787bb4fe0f83bd27ab06ea4
felitas <felita.31895@gmail.com>
2016-09-14 09:48:13 +0700

    login biar bisa enter

--- application/views/login/login.php
+++ application/views/login/login.php
@@ -31,7 +31,7 @@
 						</div>
 						<a href="<?php echo base_url('login/forget')?>" style="font-size:11px;">Forgot Password</a>
 						<div class="form-group text-center" style="margin-top:10px;">
-							<button type="button" onclick="submitForm()" id="submit" class="btn btn-primary form-control" >Login</button>
+							<input type="submit" id="submit" class="btn btn-primary form-control"value="Login">
 							
 						</div>
 
@@ -48,43 +48,39 @@
 </div>
 
 <script>
-	$("#password").keyup(function(event){
-    	if(event.keyCode == 13){
-    	 event.preventDefault();
-       	 $("#submit").click();
-    	}
-	});
+	 $(document).ready(function(){
+
+    $("#loginForm").submit(function(e){
+      data = $('#loginForm').serialize();
+      
+
+		$.ajax({
+	      url: "<?php echo base_url('login/sign_in')?>",
+	      data: data,
+	      type: 'POST',
+	      cache : false,
+	      success: function(result){
+	      	if(result == 'success'){
+	      		swal("Login Success!", "You have succesfully logged in.", "success");
+	        	setTimeout(function(){ location.replace("<?php echo base_url() ?>") }, 1000);
+	      	}else if(result == 'failed'){
+	      		swal("Login Failed!", "Your username or password is incorrect", "error");
+	        	swal({title: "Login Failed!",
+	        	     text: "Your username or password is incorrect",   
+	        	     timer: 1000,   
+	        	     showConfirmButton: false,
+	        	     type: "error" });
+	      	}
+			}
+		});	      	
+      return false;
+
+    });
+
+  });
+
 </script>
 
-<script>
-function submitForm(){
-
-	data = $('#loginForm').serialize();
-	$.ajax({
-      url: "<?php echo base_url('login/sign_in')?>",
-      data: data,
-      type: 'POST',
-      cache : false,
-      success: function(result){
-      	if(result == 'success'){
-      		swal("Login Success!", "You have succesfully logged in.", "success");
-        	setTimeout(function(){ location.replace("<?php echo base_url() ?>") }, 1000);
-      	}else if(result == 'failed'){
-      		swal("Login Failed!", "Your username or password is incorrect", "error");
-        	swal({title: "Login Failed!",
-        	     text: "Your username or password is incorrect",   
-        	     timer: 1000,   
-        	     showConfirmButton: false,
-        	     type: "error" });
-      	}
-       
-        
-      }
-      
-    });
-}
-
-</script> 
 
 <script>
 	$('#loginModal').on('shown.bs.modal', function () {
