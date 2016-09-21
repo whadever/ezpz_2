fdd17f85f51c2b3bae72c997c2e1dc3943f3ea45
nyonyos <reyner@lrmcorporation.com>
2016-09-21 18:42:40 +0700

    about us

--- application/views/main/about.php
+++ application/views/main/about.php
@@ -5,12 +5,7 @@
 	<hr style="border-top:3px double black;">
 	
 	<p style="text-align:center; margin-top:10%; font-size:2em;">
-	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+	REYNER
 	</p>
 
 	<p style="text-align:center;margin-top:10%; font-size:2em;">
